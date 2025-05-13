cd6beef1b28abc5c6d041e5f38b006c057326ca0
Sandjio <emmasandjio@gmail.com>
2025-05-13 12:11:41 +0100

    feat: add corn and gold storage images to Preloader scene

--- packages/clash-of-farms-frontend/src/game/scenes/Preloader.ts
+++ packages/clash-of-farms-frontend/src/game/scenes/Preloader.ts
@@ -31,6 +31,8 @@ export class Preloader extends Scene {
         this.load.image("trophy", "golden-trophy-medium.png");
         this.load.image("fight", "fight.png");
         this.load.image("shop", "shop.png");
+        this.load.image("corn", "corn.png");
+        this.load.image("gold-storage", "gold-storage.png");
     }
 
     create() {
